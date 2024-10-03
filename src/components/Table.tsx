6271dbcd21d0b790e01f639c070f33acd73e078b
Uttam Kumar <uttamkumar@Uttams-MacBook-Air.local>
2024-10-03 14:44:56 +0530

    sadss

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Table = ({columns , renderRow , data} : {
     columns : {header : string ; accessor : string; className?:string }[];
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //@ts-ignore
     renderRow : (item : any) => React.ReactNode;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //@ts-ignore
     data : any[];
 }) => {
   return (
